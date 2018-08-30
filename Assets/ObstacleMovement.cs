78aa93185d099ca980d94067570bcbe0bc3d0a34
Dom <djmodd@live.com>
2018-08-30 16:46:49 -0400

    Test
    Small change to practice

--- Assets/ObstacleMovement.cs
+++ Assets/ObstacleMovement.cs
@@ -8,7 +8,7 @@ public class ObstacleMovement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        print("I have made here a very big change now");
 	}
 	
 	// Update is called once per frame
