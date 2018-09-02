8cf7b6346c670e1ff754dc27478121dd6460d3c0
Unknown <djmodd@live.com>
2018-09-01 20:30:22 -0400

    Playfeild Generation in progress

--- Assets/SceneController.cs
+++ Assets/SceneController.cs
@@ -4,22 +4,72 @@ using UnityEngine;
 
 public class SceneController : MonoBehaviour {
 
-    public GameObject prefabWall;
-    float delayUntilSpawn = 0;
+    public GameObject prefabPlayFeild;
+    float feildLength = 30;
+    ArrayList playFeildArray = new ArrayList();
 
 	void Start () {
-		
-	}
+        initalizePlayfeild();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-        delayUntilSpawn -= Time.deltaTime;
-        if (delayUntilSpawn <= 0)
-        {
-            Vector3 pos = new Vector3(0, 0, 20);
-            Instantiate(prefabWall, pos, Quaternion.identity);
-            delayUntilSpawn = Random.Range(1, 3);
+        // GameObject k = (GameObject)playFeildArray[0];
+        GameObject k = (GameObject)playFeildArray[playFeildArray.Count - 1];
+       //print("k: " + k.transform.position.z);
+
+        if (k.transform.position.z <= feildLength) {
+            spawnPlayFeild();
         }
-	}
+
+        spacePlayFeild();
+        removePlayFeild();
+
+
+    }
+
+    void initalizePlayfeild() {
+        float loopAmount = feildLength + 5 / 1.1f;
+        Vector3 position = new Vector3(0, 0, -4.95f);
+        for (int i = 0; i < loopAmount; i++) {
+            GameObject p = Instantiate(prefabPlayFeild, position, Quaternion.identity);
+            playFeildArray.Add(p);
+            position.z += 1.1f;
+        }
+    }
+
+    void spawnPlayFeild() {
+        GameObject g = (GameObject)playFeildArray[playFeildArray.Count - 1];
+        //GameObject g = (GameObject)playFeildArray[0];
+        float z = g.transform.position.z + 1.1f;
+        //print("z: " + z);
+        Vector3 pos = new Vector3(0, 0, z);
+        GameObject p = Instantiate(prefabPlayFeild, pos, Quaternion.identity);
+        playFeildArray.Add(p);
+    }
+
+    void removePlayFeild() {
+        for (int i = playFeildArray.Count - 1; i > 0; i--)
+        {
+            GameObject current = (GameObject)playFeildArray[i];
+            if (current.transform.position.z <= -5) {
+                Destroy((GameObject)playFeildArray[i]);
+                playFeildArray.RemoveAt(i);
+            }
+        }
+    }
+
+
+    void spacePlayFeild() {
+       
+        for (int i = playFeildArray.Count - 1; i > 0; i--) {
+            GameObject current = (GameObject)playFeildArray[i];
+            GameObject next = (GameObject)playFeildArray[i-1];
+            print(current.transform.position.z - next.transform.position.z);
+            //if (current.transform.position.z - next.transform.position.z) {
+
+            // }
+        }
+    }
 }
