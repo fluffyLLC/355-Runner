b9e3cb3811a0ec86e9af8814213a2c8de7843fe9
Unknown <djmodd@live.com>
2018-09-01 20:35:48 -0400

    Playfeild Generation in Progress
    Made the Scene Controller script run after the Obstacle Movement script so block spacing is accurate

--- Assets/ObstacleMovement.cs
+++ Assets/ObstacleMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ObstacleMovement : MonoBehaviour {
 
-    const float speed = -20;
+    const float speed = -10;
 
 	// Use this for initialization
 	void Start () {
