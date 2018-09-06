8def5b66b1e79c2ab9f4a164135c1f85a342b807
Dom <djmodd@live.com>
2018-09-06 16:55:51 -0400

    Player Movement Begun

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,48 +6,101 @@ public class PlayerMovement : MonoBehaviour {
 
     public float laneWidth = 2;
     int lane = 0;
+    float horizontalClamp = 3.85f;
+    Vector3 Velocity;
+    float friction = 0.85f;
+    float acceleration = 4f;
     float verticalPrevious;
     float floorLock;
     float gravity;
 
+    enum MovmentState {
+        run,
+        jump,
+        glide
+
+    }
+
+    MovmentState playerState;
 
 
 
     void Start() {
-
+        playerState = MovmentState.run;
     }
 
     // Update is called once per frame
     void Update() {
 
-        float leftRight = Input.GetAxisRaw("Horizontal");
-        float jump = Input.GetAxisRaw("Vertical");
 
-        if (Input.GetButtonDown("Horizontal")) {
+        switch (playerState) {
 
-            // if pressing left a
-            if (leftRight == -1) {
-                lane--;
-            } else if (leftRight == 1) {// if pressing right d
-              
-                lane++;
-            }
-            //print(lane);
-            lane = Mathf.Clamp(lane, -1, 1);
+            case MovmentState.run:
+                DoRun();
+                break;
+            case MovmentState.jump:
+                DoJump();
+                break;
+            case MovmentState.glide:
+                DoGlide();
+                break;
+            default:
+                print("error: MovmentState out of bounds");
+                break;
         }
 
-        if (Input.GetButtonDown("Vertical")) {
-            
-        }
 
-        float targetX = lane * laneWidth;
 
-        float x = (targetX - transform.position.x) * .1f;
-        transform.position += new Vector3(x, 0, 0);
+
+
+
 
 
 
     }
+
+
+    void DoRun() {
+        float leftRight = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButton("Horizontal")) {
+            if (leftRight == -1) {//if pressing a
+                Velocity.x += -acceleration; // * Time.deltaTime;
+            } else if (leftRight == 1) {// if pressing right d
+                Velocity.x += acceleration;// * Time.deltaTime;
+            }
+        } //else {
+        Velocity.x *= friction; //* Time.deltaTime;
+        //}
+
+
+        float x = transform.position.x + Velocity.x * Time.deltaTime;
+        transform.position = new Vector3(x, 0, 0);
+
+        if (transform.position.x > horizontalClamp || transform.position.x < -horizontalClamp) {
+            print("clamping");
+            float clampPosition = Mathf.Clamp(transform.position.x, -horizontalClamp, horizontalClamp);
+            transform.position = new Vector3(clampPosition, 0, 0);
+            Velocity.x = 0;
+
+        }
+
+    }
+
+    void DoJump() {
+        // float jump = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetButtonDown("Vertical")) {
+
+        }
+    }
+
+    void DoGlide() {
+
+    }
+
+
+
 
     void OverlappingAABB(AABB other) {
         if (other.tag == "Coin") {
